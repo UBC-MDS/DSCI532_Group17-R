ccfef3d52baa997100d2a1715fa7bfe99919b382
Guanshu Tao <guanshu@ualberta.ca>
2021-01-30 21:13:57 -0700

    changed height column, changed size, drop column X

--- app.R
+++ app.R
@@ -13,6 +13,8 @@ source("data_manager.R")
 data <- read_csv('data/processed/processed_data.csv')
 colnames(data)[8] <- "Value(\u20AC)"
 colnames(data)[9] <- "Wage(\u20AC)"
+data$Height <- as.integer(substring(data$Height, 3, 5)) + 
+  12*as.integer(substring(data$Height, 1, 1))
 table <- make_table(data)
 charts <- plot_altair(data)
 
@@ -34,33 +36,40 @@ app$layout(
               dccDropdown(
                 id='rankby-widget',
                 value='Overall',
-                options=(data %>% colnames) %>% map(function(col) list(label = col, value = col))
+                options=((data %>% colnames)[2:11]) %>% map(function(col) list(label = col, value = col)),
+                style=list('width'= '80%')
                 ),
+              htmlBr(),
               htmlBr(),
               htmlDiv('Order:'),
               dccDropdown(
                 id='order-widget',
                 value='True',
                 options=list(list(label = 'Descending', value =  'False'),
-                             list(label = 'Ascending', value = 'True'))
+                             list(label = 'Ascending', value = 'True')),
+                style=list('width'= '80%')
                 ),
+              htmlBr(),
               htmlBr(),
               htmlDiv('Continent:'),
               dccDropdown(
                 id='filter-cont-widget',
                 value='',
-                options=map(unique(data$Continent), function(val) list(label = val, value = val))
+                options=map(unique(data$Continent), function(val) list(label = val, value = val)),
+                style=list('width'= '80%')
                 ),
+              htmlBr(),
               htmlBr(),
               htmlDiv('Club:'),
               dccDropdown(
                 id='filter-club-widget',
                 value='',
-                options=map(unique(data$Club[!is.na(data$Club)]), function(val) list(label = val, value = val))
+                options=map(unique(data$Club[!is.na(data$Club)]), function(val) list(label = val, value = val)),
+                style=list('width'= '80%')
               )), md=3),
           dbcCol(
             list(
-              htmlH1('FIFA STAR BOARD', style=list(width= '50vh', height= '10vh')),
+              htmlH1('FIFA STAR BOARD', style=list(textAlign = 'center', color = '#0000A0', width= '80vh', height= '9vh')),
               htmlH4('Select Attributes:'),
               dccDropdown(
                 id='attribute-widget',
@@ -69,6 +78,9 @@ app$layout(
                 multi=TRUE
                 ),
               dashDataTable(
+                style_table = list(
+                  height = '100'
+                ),
                 id = "table",
                 columns = lapply(colnames(table), 
                                  function(colName){
@@ -77,9 +89,22 @@ app$layout(
                                      name = colName
                                    )
                                  }),
-                data = df_to_list(table)
+                data = df_to_list(table),
+                style_data_conditional = list(list('if' = list(column_id = 'Ranking'),width = '50px'),
+                                              list( 'if' = list(column_id = 'Name'),width = '50px'),
+                                              list( 'if' = list(column_id = 'Nationality'), width = '50px'),
+                                              list( 'if' = list(column_id = 'Value(\u20AC)'), width = '70px'),
+                                              list('if' = list(column_id = 'Overall'), width = '75px' ),
+                                              list('if' = list(column_id = 'Age'),width = '75px'),
+                                              list('if' = list(column_id = 'Potential'),width = '75px'),
+                                              list('if' = list(column_id = 'Overall'),  width = '75px'),
+                                              list( 'if' = list(column_id = 'Club'),  width = '75px'),
+                                              list('if' = list(column_id = 'Weight(lbs)'),width = '75px'),
+                                              list('if' = list(column_id = 'Value'),width = '75px')
+                                              )
                 )
-              )),
+              )
+            ),
          dbcCol(
            list(
              htmlDiv(
@@ -98,7 +123,11 @@ app$layout(
                style=list('border-width'= '0', 'width' = '150%', 'height' = '350px')
              )
            ), md=3)
-          )))))
+          )
+         )
+        )
+      )
+    )
     
 # updates table from all 5 dropdowns
 #app$callback(
